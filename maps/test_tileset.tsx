a34e25557c127cf629ae8502a71948ffbd1bae1f
ds0 <dmitry@ds0.xyz>
2020-06-06 11:22:51 -0400

    removed unnecessary files

--- maps/test_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="test_tileset" tilewidth="10" tileheight="12" tilecount="1" columns="1">
- <image source="../assets/sprites/wall.png" width="10" height="12"/>
-</tileset>
